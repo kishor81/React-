3f893c671a6a38b2b086e1ee71eb87b275cadcd5
kishor81 <83001151+kishor81@users.noreply.github.com>
2023-07-28 13:29:39 +0545

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import FormPractice from './Practice/FormPractice';
+import PlayQuiz from './Practice/PlayQuiz';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FormPractice />} />
+          <Route path="/play-quiz" element={<PlayQuiz />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
